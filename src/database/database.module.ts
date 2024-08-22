ea7b31f0e52ea40fa07bf24a35e7dbf670e6dd16
Vinícius Vilar <viniciusvillarma7x@gmail.com>
2024-08-22 09:48:30 -0300

    Criação de variáveis de ambiente e refatoração de configuração do Banco de Dados

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -1,28 +1,25 @@
 import { Module } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Course } from 'src/courses/entities/courses.entity';
 import { Tag } from 'src/courses/entities/tags.entity';
 import { DataSourceOptions } from 'typeorm';
 
-export const dataSourceOptions: DataSourceOptions = {
-    type: 'postgres',
-    host: 'localhost',
-    port: 5432,
-    username: 'postgres',
-    password: 'docker',
-    database: 'devtraining',
-    entities: [__dirname + '/../**/*.entity.{js,ts}'],
-    synchronize: false,
-    
-}
-
 @Module({
     imports: [TypeOrmModule.forRootAsync({
-        useFactory: async () => {
+        useFactory: async (configService: ConfigService) => {
             return {
-                ...dataSourceOptions,
+                type: 'postgres',
+                host: configService.get('DB_HOST'),
+                port: Number(configService.get('DB_PORT')),
+                username: configService.get('DB_USER'),
+                password: configService.get('DB_PASS'),
+                database: configService.get('DB_NAME'),
+                entities: [__dirname + '/../**/*.entity.{js,ts}'],
+                synchronize: false,
             }
-        }
+        },
+        inject: [ConfigService]
     })]
 })
 export class DatabaseModule {}
--- src/database/orm-cli-config.ts
+++ src/database/orm-cli-config.ts
@@ -1,5 +1,16 @@
-import { DataSource } from "typeorm";
-import { dataSourceOptions } from "./database.module";
+import 'dotenv/config'
+import { DataSource, DataSourceOptions } from "typeorm";
+export const dataSourceOptions: DataSourceOptions = {
+    type: 'postgres',
+    host: process.env.DB_HOST,
+    port: Number(process.env.DB_PORT),
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    entities: [__dirname + '/../**/*.entity.{js,ts}'],
+    synchronize: false,
+    
+}
 
 export const dataSource = new DataSource({
     ...dataSourceOptions,
