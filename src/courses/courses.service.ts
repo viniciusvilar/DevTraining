948a3727502095d83e06a0e063d63fc4e192bfdb
Vinícius Vilar <viniciusvillarma7x@gmail.com>
2024-08-22 09:03:10 -0300

    Criação do teste unitário do método create de CourseService

--- src/courses/courses.service.ts
+++ src/courses/courses.service.ts
@@ -10,12 +10,10 @@ import { Tag } from './entities/tags.entity';
 @Injectable()
 export class CoursesService {
 
-    constructor (
-        @InjectRepository(Course)
-        private readonly courseRepository : Repository<Course>,
-        @InjectRepository(Tag)
-        private readonly tagRepository : Repository<Tag>
-    ) {}
+    @InjectRepository(Course)
+    private readonly courseRepository : Repository<Course>
+    @InjectRepository(Tag)
+    private readonly tagRepository : Repository<Tag>
 
     async findAll() {
         return await this.courseRepository.find({
